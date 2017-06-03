b76e3b44a18828f87b52f3257a60f6cb9ee48957
Rio <rnose512@gmail.com>
2017-06-03 09:19:35 -0700

    Fix methods and add additional loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -13,21 +13,28 @@ while i < answer
   puts 'Would you like to enroll in the company’s health insurance? (Y/N)'
   insurance = gets.chomp.capitalize
 
-  if 2017 - year === age || age-1
-    age_correct = true
-  end
-
-  if age_correct && (garlic === 'Y' || insurance === 'Y')
+  if age == 2017-year && (garlic === 'Y' || insurance === 'Y')
     puts 'Probably not a vampire.'
-  elsif !age_correct && (garlic === 'N' || insurance === 'N')
-    puts 'Probably a vampire.'
-  elsif !age_correct && (garlic === 'N' && insurance === 'N')
+  elsif age != 2017-year && (garlic === 'N' && insurance === 'N')
     puts 'Almost certainly a vampire.'
+  elsif age != 2017-year && (garlic === 'N' || insurance === 'N')
+    puts 'Probably a vampire.'
   elsif name === 'Drake Cula' || name === 'Tu Fang'
     puts 'Definitely a vampire.'
   else
     puts "Results inconclusive."
   end
 
+  allergy = nil
+  until allergy == 'done' || allergy == 'sunshine'
+    puts "What allergies do you have? (Type 'done' when there are no more allergies)"
+    allergy = gets.chomp
+    if allergy == 'sunshine'
+      puts "Probably a vampire"
+    end
+  end
+
   i += 1
 end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
