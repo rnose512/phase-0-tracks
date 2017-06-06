3b7b19c704116c7ed8187e9788a77e81bff8145a
Rio <rnose512@gmail.com>
2017-06-06 07:37:37 -0700

    Add last loop

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,7 +1,6 @@
 design = {}
-
 #Prompt the user to provide answers for the following: client's name, client's age, number of children, decor theme, likes carpet, likes hardwood, favorite wall color
-puts "Pleas tell my a bit about yourself. What is your name?"
+puts "Please tell my a bit about yourself. What is your name?"
 design[:name] = gets.chomp.capitalize
 puts "How old are you?"
 design[:age] = gets.chomp.to_i
@@ -10,21 +9,9 @@ design[:children] = gets.chomp.to_i
 puts "What would you like your decor theme to be?"
 design[:decor_theme] = gets.chomp.capitalize
 puts "Do you like carpet?"
-design[:carpet] = gets.chomp.upcase
-if design[:carpet] == "YES"
-  design[:carpet] = true
-else
-  design[:carpet] = false
-end
+design[:carpet] = gets.chomp
 puts "Do you like hardwood?"
-design[:hardwood] = gets.chomp.upcase
-if design[:hardwood] == "YES"
-design[:hardwood]
-  design[:hardwood] = true
-else
-design[:hardwood]
-  design[:hardwood] = false
-end
+design[:hardwood] = gets.chomp
 puts "What is your favorite wall color?"
 design[:color] = gets.chomp
 
@@ -32,9 +19,20 @@ design[:color] = gets.chomp
 design.each {|key, value| puts "#{key} : #{value}" }
 
 #ask the user if they want to change any of the answers, then change it if they do
-# puts "Would you like to make any changes to your answers?"
+answer = gets.chomp.upcase
+if answer == "Y"
+
+elsif answer == "N"
+end
+
+loop do
+  puts "If you would like to update one of your preferences, please tell us what you would like to update. Otherwise, type 'none'."
+  input = gets.chomp.intern
+  break if input == :none
+  puts "What is your new value?"
+  new_value = gets.chomp
+  design[input] = new_value
+end
 
 #print the last version of the hash
-# p design
-
-#exit the program
+design.each {|key, value| puts "#{key} : #{value}" }
