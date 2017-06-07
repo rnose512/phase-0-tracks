1fc5cdbd7d92725d6bfad9748ebcc4cb01ef0026
Rio <rnose512@gmail.com>
2017-06-06 22:07:27 -0700

    Initial commit

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,26 @@
+vowels = 'aeiou'
+consonants = ''
+
+def next_vowel
+end
+
+def next_consonant
+
+end
+
+# ask user for their name
+puts 'What is your name?'
+name = gets.chomp.downcase
+# swap first and last name
+split_name = name.split(" ")
+swap_name = split_name[1] + " " + split_name[0]
+p swap_name
+
+# change vowels to the next vowel in 'aeiou'
+# change consonants to the next consonant
+
+def fake_name(name)
+#make sure to capitalize at the end
+end
+
+fake_name("Felicia Torres")
