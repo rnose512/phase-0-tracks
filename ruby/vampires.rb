9acac28b6accb93c669459e8b67f22a502b4afdf
Rio <rnose512@gmail.com>
2017-06-11 13:01:25 -0700

    Fix program using feedback from Glenna

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,29 +1,30 @@
 puts "How many employees will be processed?"
 answer = gets.chomp.to_i
 i = 0
+current_year = Time.new.year
 while i < answer
   puts 'What is your name?'
   name = gets.chomp
   puts 'How old are you?'
-  age = gets.chomp.to_i
+  employee_age = gets.chomp.to_i
   puts 'What year were you born?'
-  year = gets.chomp.to_i
+  employee_birth_year = gets.chomp.to_i
   puts 'Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)'
   garlic = gets.chomp.capitalize
   puts 'Would you like to enroll in the company’s health insurance? (Y/N)'
   insurance = gets.chomp.capitalize
 
-  if age == 2017-year && (garlic === 'Y' || insurance === 'Y')
-    puts 'Probably not a vampire.'
-  elsif age != 2017-year && (garlic === 'N' && insurance === 'N')
-    puts 'Almost certainly a vampire.'
-  elsif age != 2017-year && (garlic === 'N' || insurance === 'N')
-    puts 'Probably a vampire.'
-  elsif name === 'Drake Cula' || name === 'Tu Fang'
-    puts 'Definitely a vampire.'
-  else
-    puts "Results inconclusive."
-  end
+  likes_garlic = garlic == 'Y'
+  needs_insurance = insurance == 'Y'
+  correct_age = employee_age == current_year - employee_birth_year || employee_age == (current_year - 1) - employee_birth_year
+
+  result = ''
+  result = 'Probably not a vampire.' if correct_age && (likes_garlic || needs_insurance)
+  result = 'Almost certainly a vampire.' if !correct_age && (!likes_garlic && !needs_insurance)
+  result = 'Probably a vampire.' if !correct_age && (!likes_garlic || !needs_insurance)
+  result = 'Definitely a vampire.' if name === 'Drake Cula' || name === 'Tu Fang'
+  result = 'Results inconclusive.' if result == ''
+  p result
 
   allergy = nil
   until allergy == 'done' || allergy == 'sunshine'
