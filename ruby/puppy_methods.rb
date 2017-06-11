f3ccfbcb0d6efb79468ff02b99301465b98d596e
Rio <rnose512@gmail.com>
2017-06-11 15:10:41 -0700

    Release 1 and 2. Create new class with methods, data structure, and call methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,4 @@
+# Release 0: Add methods to Puppy Class and add driver code to test methods
 class Puppy
 
   def fetch(toy)
@@ -29,3 +30,27 @@ sam.speak(4)
 sam.roll_over
 sam.dog_years(3)
 sam.paw
+
+class FamousClimbers
+
+  def initialize(name)
+    @name = name
+  end
+
+  def climb(grade)
+    p "I'm going to climb a #{grade}"
+  end
+
+  def fall
+    p "Catch!"
+  end
+
+end
+
+climbers = {}
+climbers = 50.times.collect {FamousClimbers.new(50)}
+
+climbers.each do |climber|
+  climber.climb("V15")
+  climber.fall
+end
