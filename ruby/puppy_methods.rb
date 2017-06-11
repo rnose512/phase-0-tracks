0550f2c88ec0ca0e09bb894c16c068c32245b579
Rio <rnose512@gmail.com>
2017-06-11 14:50:33 -0700

    Release 0: Add more methods to Puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,7 +5,27 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    p "Woof!" * int
+  end
+
+  def roll_over
+    p "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    p human_years * 15
+  end
+
+  def paw
+    p "*Puts paw on human palm*"
+  end
+
 end
 
 sam = Puppy.new
 sam.fetch("ball")
+sam.speak(4)
+sam.roll_over
+sam.dog_years(3)
+sam.paw
