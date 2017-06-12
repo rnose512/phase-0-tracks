917ab0c7f9aced5d78342473dedb0c965a518548
Rio <rnose512@gmail.com>
2017-06-12 12:52:28 -0700

    Add setter and getter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,7 @@
 # Define a Santa class, include initialize method as well as 2 other instance methods
 # Release 1: Add attributes to Santa Class
 # Then, initialize Santa instances
-# Release 2:
+# Release 2: Work with getter and setter methods
 
 class Santa
 
@@ -21,17 +21,45 @@ class Santa
     p "That was a good #{cookie_type}!"
   end
 
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking.delete_at(@reindeer_ranking.index(reindeer_name))
+    @reindeer_ranking[-1] = reindeer_name
+  end
+
+  #setter method
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
+  #getter method
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
 end
 
 sally = Santa.new("female", "South African")
 sally.speak
 sally.eat_milk_and_cookies("oatmeal cookie")
+sally.celebrate_birthday
+sally.get_mad_at("Rudolph")
+sally.gender=("Transgender")
+p sally.ethnicity
+p sally.reindeer_ranking
 
-santas = []
-example_genders = ["female", "male", "N/A", "Hijira", "Genderless"]
-example_ethnicities = ["Japanese", "French", "Prefer not to say", "Micronesian", "Polynesian"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
 
-p santas
+# santas = []
+# example_genders = ["female", "male", "N/A", "Hijira", "Genderless"]
+# example_ethnicities = ["Japanese", "French", "Prefer not to say", "Micronesian", "Polynesian"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+
