3ed380f51b7ea1cffa01946760e3e1f44e425f79
Rio <rnose512@gmail.com>
2017-06-13 19:59:38 -0700

    Add module methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,2 +1,18 @@
+# Declare Shout module
+# Define a method that takes words and makes them sound angry
+# Define the method on the self keyword, since this is a standalone module
 module Shout
+
+  def self.yell_angrily(words)
+    puts words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+    puts words + "!!!" + " :)"
+  end
+
 end
+
+# Driver code
+Shout.yell_angrily("No")
+Shout.yell_happily("YAYYY")
