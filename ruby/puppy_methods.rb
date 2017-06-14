ddc7b6f182e090594db03747c27a2f57b1befb3b
Rio <rnose512@gmail.com>
2017-06-13 17:45:31 -0700

    Create Pokemon Class and add driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -36,3 +36,40 @@ puppy.speak(4)
 puppy.roll_over
 puppy.dog_years(3)
 puppy.sit
+
+class Pokemon
+
+  attr_reader :name
+
+  def initialize(name)
+    puts "Initializing new Pokemon instance ..."
+    @name = name
+  end
+
+  def summon
+    puts "#{name}, I choose you!"
+  end
+
+  def attack(damage)
+    puts "#{name} did #{damage} HP to the opponent"
+  end
+
+end
+
+# Driver code
+
+pokemon = []
+sample_pokemon = ["Pikachu", "Togepi", "Squirtle", "Bulbasaur", "Charmander", "Mew", "Snorlax", "Jigglypuff", "Diglet", "Coughing", "Ekans", "Meowth", "Magicarp", "Garados"]
+
+50.times do
+  pokemon << Pokemon.new(sample_pokemon.sample)
+end
+
+pokemon.each do |pokemon|
+  pokemon.summon
+  pokemon.attack(rand(10..200))
+end
+
+# pokemon = Pokemon.new("Pikachu")
+# pokemon.summon
+# pokemon.attack(50)
