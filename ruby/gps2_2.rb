1d148b9cb05319ec22ae7a564765d326a2168e41
Rio <rnose512@gmail.com>
2017-06-14 07:53:46 -0700

    Add reflection notes

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -11,9 +11,7 @@
 
 def create_list(items)
   groceries = {}
-  items.split(" ").each do |item|
-    groceries[item] = 1
-  end
+  items.split(" ").each { |item| groceries[item] = 1 }
   print_list(groceries)
   groceries
 end
@@ -24,7 +22,7 @@ end
 #   Set item name as list hash key and optional quantity as list hash value
 # output: List hash with the new item
 
-def add_item(list, item, optional_quantity)
+def add_item(list, item, optional_quantity = 1)
   list[item] = optional_quantity
 end
 
@@ -35,6 +33,7 @@ end
 # output: List hash with item removed
 
 def remove_item(list, item)
+  puts "This item does not exist in your list" if !list.include?(item)
   list.delete(item)
   list
 end
@@ -46,7 +45,11 @@ end
 # output: List hash with updated item quantity
 
 def update_quantity(list, item, new_quantity)
-  list[item] = new_quantity
+  if list.include?(item)
+    list[item] = new_quantity
+  else
+    puts "#{item} does not exist, did you mean to add it?"
+  end
   list
 end
 
@@ -59,16 +62,39 @@ end
 
 def print_list(list)
   puts "Here is your grocery list:"
-  list.each do |item, quantity|
-    puts "#{item} : #{quantity}"
-  end
+  list.each { |item, quantity| puts "#{item} : #{quantity}" }
 end
 
 # Driver code:
 groceries = create_list("Apples Oranges Pears Tomatoes")
-add_item(groceries, "Cucumbers", 5)
+add_item(groceries, "Lemonade", 2)
+add_item(groceries, "Tomatoes", 3)
+add_item(groceries, "Onions", 1)
+add_item(groceries, "Ice Cream", 4)
+add_item(groceries, "Pizza")
 p groceries
-remove_item(groceries, "Oranges")
+remove_item(groceries, "Lemonade")
+remove_item(groceries, "Papaya")
 p groceries
-update_quantity(groceries, "Apples", 10)
+update_quantity(groceries, "Ice Cream", 1)
+update_quantity(groceries, "Figs", 3)
 p groceries
+print_list(groceries)
+
+# What did you learn about pseudocode from working on this challenge?
+# Writing good pseudocode in the beginning makes it really easy to write the actual code
+
+# What are the tradeoffs of using arrays and hashes for this challenge?
+# Hashes make it easier since you can have more information but with less code
+
+# What does a method return?
+# A method returns whatever code is passed inside the method. Sometimes you'd have to return something at the end, otherwise it will return bits of code that may not be useful to somebody who uses the code later.
+
+# What kind of things can you pass into methods as arguments?
+# Strings, booleans, integers, nil
+
+# How can you pass information between methods?
+# You can use methods within a method, but you will need to make sure to define variables outside of the method, sometimes assigning return values of methods to a global variable so you can use it in other methods.
+
+# What concepts were solidified in this challenge, and what concepts are still confusing?
+# I'm getting a better grasp of return values and the difference between return and p. Helin was very good at explaining it.
