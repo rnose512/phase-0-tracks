4ba396f97c53083fd0df01aee250d8c5a33d068b
Rio <rnose512@gmail.com>
2017-06-19 12:48:55 -0700

    Add code and driver code for reverse method

--- js/explore.js
+++ js/explore.js
@@ -7,3 +7,20 @@
   // add the index of the string to the reverse string variable
 // ouput: string reversed
 
+function reverseString(string) {
+  reversedString = ''
+  for (var i = 0; i < string.length; i++) {
+    reversedString = string[i] + reversedString;
+  }
+  return reversedString;
+}
+
+var noMatter = reverseString("No matter what");
+if (1 + 1 == 2) {
+  console.log(noMatter);
+}
+
+var never = reverseString("Never give up");
+if (9 % 3 === 0) {
+  console.log(never);
+}
