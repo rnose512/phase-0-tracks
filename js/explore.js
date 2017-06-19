cfc9e0745f3b0938146bcd758b477212843ae3bb
Rio <rnose512@gmail.com>
2017-06-19 12:48:25 -0700

    Add pseudo code

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// Write a function that reverses a string
+// input: string
+// steps:
+  // declare an empty reversed string variable
+  // declare an index variable and assign it 0
+  // while the index is less than the length of the string
+  // add the index of the string to the reverse string variable
+// ouput: string reversed
+
