f03d9dc58494cff311900f36fe6999e48be16b71
Rio <rnose512@gmail.com>
2017-06-21 17:55:44 -0700

    Create car construct and create car objects

--- js/data_structures.js
+++ js/data_structures.js
@@ -14,3 +14,28 @@ function createHorse() {
 }
 
 createHorse();
+
+// Write constructor function for car
+// Give it a few different properties of various data types
+// include at least 1 function
+// Demonstrate that functions work by creating a few cars
+
+function Car(model, year, color, fourWheelDrive) {
+  this.model = model;
+  this.year = year;
+  this.color = color;
+  this.fourWheelDrive = fourWheelDrive;
+
+  this.horn = function() {console.log("BEEP!"); };
+
+}
+
+var anotherCar = new Car("Sonata", 2014, "white", false);
+console.log(anotherCar);
+anotherCar.horn();
+
+console.log("-------------");
+
+var secondCar = new Car("Beetle", 1995, "yellow", false);
+console.log(secondCar);
+secondCar.horn();
