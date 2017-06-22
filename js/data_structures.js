48b166144f5ac45f0ea31cc24b6095c9d9fab23a
Rio <rnose512@gmail.com>
2017-06-21 17:39:26 -0700

    Assign colors to horses

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,3 +4,13 @@ var horseNames = ["Ed", "Charlie", "Donald", "Shirley"];
 colors.push("yellow");
 horseNames.push("Stephanie");
 
+function createHorse() {
+  for (var i = 0; i < horseNames.length; i++) {
+    var horse = new Object();
+    horse.name = horseNames[i];
+    horse.color = colors[i];
+    console.log(horseNames[i] + " is " + colors[i]);
+  }
+}
+
+createHorse();
