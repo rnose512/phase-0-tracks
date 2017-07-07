444796d0c23f68b36ab6234369203ad08ea536c3
Rio <rnose512@gmail.com>
2017-07-06 22:14:14 -0700

    Refactor code by breaking down into smaller methods

--- databases/sql_and_ruby/users.rb
+++ databases/sql_and_ruby/users.rb
@@ -1,4 +1,3 @@
-# require gems
 require 'sqlite3'
 
 # create SQLite3 database
@@ -13,6 +12,14 @@ $weekdays = {
   "saturday" => 6
 }
 $t = Time.now
+$user_data = {
+  name: '',
+  username: '',
+  email: '',
+  task: '',
+  location: '',
+  due_date: 0
+}
 
 # create users and tasks table
 create_users_table_cmd = <<-SQL
@@ -20,8 +27,7 @@ create_users_table_cmd = <<-SQL
     id INTEGER PRIMARY KEY,
     name VARCHAR(100),
     username VARCHAR(100),
-    email VARCHAR(100),
-    account_confirmed BOOLEAN
+    email VARCHAR(100)
   )
 SQL
 
@@ -37,100 +43,148 @@ create_tasks_table_cmd = <<-SQL
   )
 SQL
 
- db.execute(create_users_table_cmd)
- db.execute(create_tasks_table_cmd)
+db.execute(create_users_table_cmd)
+db.execute(create_tasks_table_cmd)
 
-# method to add a user to users table
-def create_user(db, username, name, email)
-  db.execute("INSERT INTO users (name, username, email) VALUES (?, ?, ?)", [name, username, email])
+def get_new_user_input
+  puts "What is your name?"
+  $user_data[:name] = gets.chomp.capitalize
+  puts "What would you like your user name to be?"
+  $user_data[:username] = gets.chomp.downcase
+  puts "What is your email?"
+  $user_data[:email] = gets.chomp
+end
+
+def create_user(db)
+  db.execute("INSERT INTO users (name, username, email) VALUES (?, ?, ?)", $user_data[:name], $user_data[:username], $user_data[:email])
+end
+
+# method to get user_id
+def get_user_id(db)
+  db.execute("SELECT id FROM users WHERE username = '#{$user_data[:username]}'")[0][0]
 end
 
 # method to add task to tasks table
-def enter_task(db, input, task, location, past_due, due_date)
-  user_id_array = db.execute("SELECT id FROM users WHERE username = '#{input}'")
-  user_id = user_id_array[0][0]
-  db.execute("INSERT INTO tasks (task, location, past_due, due_date, user_id) VALUES (?, ?, ?, ?, ?)", [task, location, past_due, due_date, user_id])
+def enter_task(db)
+  user_id = get_user_id(db)
+  db.execute("INSERT INTO tasks (task, location, past_due, due_date, user_id) VALUES (?, ?, ?, ?, ?)", [$user_data[:task], $user_data[:location], $user_data[:past_due], $user_data[:due_date], user_id])
 end
 
- def print_tasks(db, input)
-  # update past_due column if task is now past due
-  user_id = db.execute("SELECT id FROM users WHERE username = '#{input}'")[0][0]
-  due_date_array = db.execute("SELECT due_date FROM tasks WHERE user_id = #{user_id}")
-  if !due_date_array.empty?
-    puts "Here are your tasks:"
-      due_date = due_date_array[0][0]
-      if $t.wday > due_date
-        db.execute("UPDATE tasks SET past_due = 'true' WHERE user_id = #{user_id}")
-      elsif $t.wday <= due_date
-        db.execute("UPDATE tasks SET past_due = 'false' WHERE user_id = #{user_id}")
-    end
-  end
-  # list out tasks and whether they are past due or not
+# method to display tasks
+def display_tasks(db)
+  user_id = get_user_id(db)
   tasks = db.execute("SELECT task, past_due FROM tasks WHERE user_id = #{user_id}")
-  tasks.each do |task|
-    if task[1] === "false"
-      puts "#{task[0]}"
-    elsif task[1] === "true"
-      puts "#{task[0]} (PAST DUE)"
+  puts "Here are your tasks:"
+  i = 1
+  while i < tasks.length do
+    tasks.each do |task|
+      if task[1] === "false"
+        puts "#{i} : #{task[0]}"
+      elsif task[1] === "true"
+        puts "#{i} : #{task[0]} (PAST DUE)"
+      end
+    i += 1
     end
   end
- end
-
-def add_task(db, username)
-    puts "What is your task?"
-    task = gets.chomp
-    puts "What is the location of the task?"
-    location = gets.chomp
-    # grab due date INT from weekdays hash
-    puts "What week day is the task due?"
-    due_weekday = gets.chomp.downcase
-    due_date = $weekdays[due_weekday]
-    if $t.wday <= due_date
-      past_due = 'false'
-    elsif $t.wday > due_date
-      past_due = 'true'
-    end
-
-    enter_task(db, username, task, location, past_due, due_date)
-    print_tasks(db, username)
 end
+
+def get_user_tasks(db)
+  user_id = get_user_id(db)
+  tasks = db.execute("SELECT task FROM tasks where user_id = #{user_id}")
+end
+
+# method to get task id from user
+def get_task_to_remove(db)
+  user_tasks = get_user_tasks(db)
+  puts "Please select a task:"
+  user_tasks.each {|task| puts task}
+  task_to_remove = gets.chomp
+end
+
+# method to remove task from tasks table
+def remove_task(db)
+  task_to_remove = get_task_to_remove(db)
+  db.execute("DELETE FROM tasks WHERE task = '#{task_to_remove}'")
+end
+
+def update_past_due(db)
+# update past_due column if task is now past due
+# user_id = db.execute("SELECT id FROM users WHERE username = '#{input}'")[0][0]
+  user_id = get_user_id(db)
+  due_date_array = db.execute("SELECT due_date FROM tasks WHERE user_id = #{user_id}")
+  $user_data[:due_date] = due_date_array[0][0]
+  if !due_date_array.empty?
+    if $t.wday > $user_data[:due_date]
+      db.execute("UPDATE tasks SET past_due = 'true' WHERE user_id = #{user_id}")
+    elsif $t.wday <= $user_data[:due_date]
+      db.execute("UPDATE tasks SET past_due = 'false' WHERE user_id = #{user_id}")
+    end
+  end
+end
+
+def get_user_task_input
+  puts "What is your task?"
+  $user_data[:task] = gets.chomp
+  puts "What is the location of the task?"
+  $user_data[:location] = gets.chomp
+  # grab due date INT from weekdays hash
+  puts "What week day is the task due?"
+  due_weekday = gets.chomp.downcase
+  $user_data[:due_date] = $weekdays[due_weekday]
+  if $t.wday <= $user_data[:due_date]
+    $user_data[:past_due] = 'false'
+  elsif $t.wday > $user_data[:due_date]
+    $user_data[:past_due] = 'true'
+  end
+end
+
+def add_task(db)
+  get_user_task_input
+  enter_task(db)
+end
+
+def add_task_loop
+  response = ''
+  until response === 'quit'
+    puts "Would you like to add a task? ('y' or 'quit')"
+    response = gets.chomp.downcase
+    if response === 'y'
+      add_task(db)
+    end
+    response
+  end
+end
+
+# def run_script2(db)
+#   username = get_username
+
+#   if username === 'none'
+#     create_new_user
+#     prompt_new_user_to_add_task
+#   else
+#     welcome_existing_user_back
+#     display_menu_for_existing_user
+# end
 
 def run_script(db)
   puts "What is your username? Enter 'none' if you're a new user"
-  username = gets.chomp
-  if username === 'none'
-    puts "What is your name?"
-    input_name = gets.chomp
-    puts "What would you like your user name to be?"
-    input_username = gets.chomp
-    puts "What is your email?"
-    input_email = gets.chomp
-
-    create_user(db, input_username, input_name, input_email)
-  puts "Would you like to add a task? (y/n)"
-  response = gets.chomp.downcase
-    if response === 'y'
-      add_task(db, username)
-    elsif response === 'n'
-      puts "Okay, have a great day!"
-    end
+  $user_data[:username] = gets.chomp
+  if $user_data[:username] === 'none'
+    get_new_user_input
+    create_user(db)
+    add_task_loop
   else
-    puts "Welcome back #{username}!"
+    puts "Welcome back #{$user_data[:username]}!"
     puts "Would you like to (choose a number): \n 1. view your tasks \n 2. add a new task \n 3. remove a task "
     number = gets.chomp.to_i
     if number === 1
-      print_tasks(db,username)
+      display_tasks(db)
     elsif number === 2
-      add_task(db, username)
-    # elsif number === 3
-    #   remove_task(db,username)
+      add_task(db)
+    elsif number === 3
+      remove_task(db)
     end
   end
 end
 
 run_script(db)
-
-# db.execute("INSERT INTO users (username, name, email, account_confirmed) VALUES ('pogostick', 'Pogo Silian', 'pogo@pogostick.com', 'true')")
-
-# users = db.execute("SELECT * FROM users")
-# p users
