532b40fecb38c251b310f0fdb443f8c19b5a6f3c
Rio <rnose512@gmail.com>
2017-07-11 21:27:20 -0700

    Operate two variables and display in input

--- web_dev/jquery/calculator/script.js
+++ web_dev/jquery/calculator/script.js
@@ -2,11 +2,15 @@ $(document).ready(function() {
   var operatorButtons = ["+", "-", "*", "/"];
   var x = 0;
   var y = 0;
+  var operatorInput = null;
+  var outputNumber = 0;
   var operator = '';
-  var flag=false;
-  var operatorConversion = {
-    "+"
-  }
+  var math = {
+    '+': function(x, y) {return x + y},
+    '-': function(x, y) {return x - y},
+    '*': function(x, y) {return x * y},
+    '/': function(x, y) {return x / y}
+  };
 
   // create calculator div
   $("body").append("<div id='calculator'></div>");
@@ -28,8 +32,7 @@ $(document).ready(function() {
     $("#calculator").append(`<button id='${operatorButtons[i]}' class='modifier operator'>${operatorButtons[i]}</button>`)
   }
 
-  // create clear and equal button
-  $("#calculator").append("<button id='.' class='modifier'>.</button>");
+  // create decimal, clear, and equal button
   $("#calculator").append("<button id='equals'>=</button>");
   $("#calculator").append("<button id='clear'>C</button>");
 
@@ -42,49 +45,45 @@ $(document).ready(function() {
 
   // on click on the divs, it will show up in the line to calculate
   $(".modifier").on("click", function(){
-    var input = this.id;
+    input = this.id;
     $("#display").val($("#display").val()+input);
   });
 
-  $(".operator").on("click", function() {
-    flag=true;
-    $(".number").on("click", function(){
+  $(".number").on("click", function(){
+    console.log("number on click");
+    if(operatorInput === null){
+      var addToX = this.id;
+      x += addToX;
+      x = Number(x);
+      console.log("X is " + x);
+     }else{
       var addToY = this.id;
       y += addToY;
+      y = Number(y);
+      outputNumber = math[operatorInput](x, y);
       console.log("Y is " + y);
-    });
+     }
+   });
+
+  $(".operator").on("click", function() {
+    console.log("operator on click");
+    operatorInput = this.id;
   });
 
-  if(flag===false){
-    $(".number").on("click", function(){
-       var addToX = this.id;
-       x += addToX;
-       console.log(flag);
-       console.log("X is " + x);
-     });
-  }
 
   // clear button
   $("#clear").on("click", function(){
     $('#display').val('');
     x = 0;
     y = 0;
+    operatorInput = null;
+    outputNumber = 0;
   });
 
   $("#equals").on("click", function(){
-    x
+    outputNumber = math[operatorInput](x, y);
+    console.log(outputNumber);
+    $("#display").val($("#display").val()+"=");
+    $("#display").val($("#display").val()+outputNumber);
   });
-
-    // until = is clicked
-    // until operator button is clicked, store numbers in x
-    // until = is clicked, store numbers in y
-    // calculate x + operator + y
-    // function calculate(){
-    //   $("#equals").on("click", function(){
-    //   });
-    // }
-
-    // calculate();
-
-    // until one of the operator buttons are pressed, add the numbers to variable X. after an operator button is pressed, add numbers to variable Y. When "=" is clicked, calculate x + operator + y
 });
